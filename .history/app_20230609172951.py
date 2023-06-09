e421ede8c158b108b4da9d2ece88fb4bd34aa060
JackChen <799904657@qq.com>
2023-06-09 17:31:29 +0800

    change port

--- .history/app_20230609172951.py
+++ .history/app_20230609172951.py
@@ -198,4 +198,4 @@ def update_graph(timer):
 
  
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051,use_reloader=False)
+    app.run_server(debug=True, host='0.0.0.0',port=8000,use_reloader=False)
